acf3b453b7531e451540a2a57b1d316d6087ceb2
ComunicacionItecnos <90643612+ComunicacionItecnos@users.noreply.github.com>
2024-03-11 16:49:08 -0600

    fix: socket server

--- index.js
+++ index.js
@@ -16,15 +16,11 @@ const main = require('./libs/mongodb');
 require('./utils/auth');
 require('./utils/notifications/firebase');
 
-app.use(cors());
-app.use(express.json({ limit: '1000mb' }));
-// app.use(express.urlencoded({ extended: true, limit: '1000mb' }));
-
 //Config socket.io
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: config.socketCLientUrl,
     methods: ['GET', 'POST'],
     allowedHeaders: ['my-custom-header'],
     credentials: true,
@@ -32,6 +28,10 @@ const io = socketIo(server, {
 });
 
 app.io = io;
+
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true, limit: '1000mb' }));
 
 app.get('/', (req, res) => {
   res.json('Hi Filibustero');
