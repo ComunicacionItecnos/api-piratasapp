7df84055d562b79ea1911c2c28fac4639dba440f
ComunicacionItecnos <90643612+ComunicacionItecnos@users.noreply.github.com>
2024-03-11 16:49:08 -0600

    fix: socket server

--- config/config.js
+++ config/config.js
@@ -8,6 +8,7 @@ const config = {
   keyCrypto: process.env.KEY_CRYPTO,
   oneSignalAppId: process.env.ONESIGNAL_APP_ID,
   oneSignalUserKey: process.env.ONESIGNAL_USER_KEY,
+  socketCLientUrl: process.env.SOCKET_CLIENT_URL,
 };
 
 module.exports = config;
